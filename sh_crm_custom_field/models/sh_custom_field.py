308df8dbfd9b3f94cce865172f667866552268c6
Dhruv Gambhava <dhruv@softhealer.com>
2025-03-13 12:28:47 +0530

    Delete sh_crm_custom_field directory

--- sh_crm_custom_field/models/sh_custom_field.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# Part of Softhealer Technology.
-
-from odoo import fields, models
-
-class CustomField(models.Model):
-    _inherit = 'crm.lead'
-    
-    more_info = fields.Text(string='More Information')
